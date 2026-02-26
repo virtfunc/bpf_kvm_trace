671582e74eb1d7bde9d1d42cdcdcfd34b6c1f923
virtfunc <admin@virtfunc.com>
2026-02-26 08:02:27 -0800

    fix build to not install deps every time lmao

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-# 0. Install dependencies for Arch Linux
-echo "Installing/updating dependencies..."
-sudo pacman -Syu --noconfirm clang gcc linux-headers libbpf elfutils zlib
+# 0. Install dependencies for Arch Linux, checking if they are already installed
+dependencies=("clang" "gcc" "linux-headers" "libbpf" "elfutils" "zlib")
+for dep in "${dependencies[@]}"; do
+    if ! pacman -Qsq "$dep" >/dev/null; then
+        echo "Installing $dep..."
+        sudo pacman -Syu --noconfirm "$dep"
+    fi
+done
 
 # 1. Generate vmlinux.h
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
@@ -15,7 +20,7 @@ clang -g -O2 -target bpf -D__TARGET_ARCH_x86 -c msr_trace.bpf.c -o msr_trace.bpf
 bpftool gen skeleton msr_trace.bpf.o > msr_trace.skel.h
 
 # 4. Compile Userspace
-gcc -g -O2 -Wall msr_trace.c -lbpf -lelf -lz -o msr_trace
+clang -g -O2 -Wall msr_trace.c -lbpf -lelf -lz -o msr_trace
 
 # 5. Run
 sudo ./msr_trace -d
