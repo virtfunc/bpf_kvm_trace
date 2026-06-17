2bf7bbc04092bc09f3377e5d3b019fde59ea02a5
virtfunc <admin@virtfunc.com>
2026-06-17 13:57:39 -0700

    increase buffer size dramatically to hopefully never lose events due to full ring buffer again. this can be reduced in memory starved enviornments

--- kvm_trace.bpf.c
+++ kvm_trace.bpf.c
@@ -50,7 +50,7 @@ struct trace_event_raw_kvm_cpuid {
 // Maps
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 256 * 1024);
+    __uint(max_entries, 32 * 1024 * 1024);
 } rb SEC(".maps");
 
 struct {
